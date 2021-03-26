0b77c91ac256a855429a59d057169f8860a7d191
Tahirintsoa Ulrich <rtfulrich@gmail.com>
2021-03-26 12:52:09 +0300

    Improvements in viewing a free chapter

--- pages/fampianarana/[courseSlug]/[chapterSlug]/index.js
+++ pages/fampianarana/[courseSlug]/[chapterSlug]/index.js
@@ -5,16 +5,33 @@ import { REVALIDATE } from '../../../../_constants/nextConstants';
 import { API_URL } from '../../../../_constants/URLs';
 import PostContent from "../../../../_components/front/PostContent";
 import { useRouter } from 'next/router';
-import UserContext from '../../../../_react-contexts/user-context';
+import Link from "next/link";
 
-export default function ViewFreeChapter({ chapter, unauthorized }) {
-	if (chapter) console.log("chapter", chapter);
+export default function ViewFreeChapter({ chapter, groups = [], unauthorized }) {
+	// if (chapter) console.log("chapter", chapter);
+
+	if (groups) {
+		groups = groups.sort((a, b) => {
+			if (a.rank < b.rank) return -1;
+			else if (a.rank === b.rank) return 0;
+			else return 1;
+		});
+		groups.forEach(group => {
+			group.chapters = group.chapters.sort((a, b) => {
+				if (a.rank < b.rank) return -1;
+				else if (a.rank === b.rank) return 0;
+				else return 1;
+			});
+		})
+		// console.log(groups);
+	}
 
 	const router = useRouter();
 
 	// M O U N T  E F F E C T
 	React.useEffect(() => {
 		if (unauthorized) router.replace(unauthorized.redirect);
+		/* ------------------------------------------------------ */
 	}, []);
 
 	if (unauthorized) return <div className="text-4xl h-full flex justify-center items-center font-bold tracking-widest">Redirecting ...</div>
@@ -22,12 +39,50 @@ export default function ViewFreeChapter({ chapter, unauthorized }) {
 	const videoURL = chapter?.video.url;
 	const videoDuration = chapter?.video.duration;
 	return (
-		<div className="px-4 md:px-8">
-			{videoURL && <div className="flex justify-center">
-				<ReactPlayer url={videoURL} />
-			</div>}
-			<div className="my-8">
-				<PostContent content={chapter?.content} />
+		<div className="px-4 md:pl-8 md:pr-2">
+			<div className="my-8 grid grid-cols-12 gap-4 relative">
+				<div className="col-span-12 md:col-span-9">
+					{videoURL && <div className="flex justify-center mb-8 bg-gray-300">
+						<ReactPlayer url={videoURL} />
+					</div>}
+					<div>
+						<PostContent content={chapter?.content} />
+					</div>
+				</div>
+				<div className="hidden md:block md:col-span-3">
+
+					{groups.map(group => (
+						<div key={group.id} className="mb-2">
+							<h3 className="py-1 twitter-bg font-bold text-center text-black">{group.title}</h3>
+							<div>
+								{group.chapters.map(chapter => {
+									const matched = !!router.asPath.match(chapter.slug);
+									const { canBeSeen } = chapter;
+									// console.log(chapter.title, canBeSeen);
+									const { courseSlug } = router.query;
+									return (
+										<div
+											className={`flex items-center transition-all duration-200 ease-in-out px-1 my-1 cursor-pointer ${matched ? "success-bg" : `${canBeSeen ? "hover:bg-gray-300 hover:text-black" : "hover:text-red-400"}`}`}
+											key={chapter.id} title={matched ? "Efa io amy pejy io !" : (canBeSeen ? "" : "Hoan zay nividy ihany !")}
+										>
+											<div className={`w-6 ${!canBeSeen ? "text-red-500" : ""}`}>{chapter.rank}</div>
+											<div className="flex-1">
+												<h2 className="text-xs font-bold">
+													{matched || !canBeSeen
+														? chapter.title
+														: <Link href={`/fampianarana/${courseSlug}/${chapter.slug}`}>
+															<a className="inline-block h-full w-full">{chapter.title}</a>
+														</Link>
+													}
+												</h2>
+											</div>
+										</div>
+									)
+								})}
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	)
@@ -64,14 +119,15 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
 	try {
 		const response = await axios.get(`${API_URL}/course/${params.courseSlug}/chapter/${params.chapterSlug}`);
-		const { chapter } = response.data;
+		const { chapter, groups } = response.data;
 
 		return {
 			props: {
 				page: {
 					title: chapter.title
 				},
-				chapter
+				chapter,
+				groups
 			},
 			revalidate: REVALIDATE
 		}
